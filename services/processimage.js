a59e317e2289d99f80814fd471d104f7c35d8f10
Michael Steidl <mwsteidl@newsit.biz>
2019-03-02 20:43:49 +0100

    Hotfix not closed exiftool process

--- services/processimage.js
+++ services/processimage.js
@@ -75,6 +75,11 @@ function processImageFileAsHtml (res, imgfpath, imgwsfpath, imgtitle, imgurl, im
                 break;
         }
     }
+    ep.close().then(() => {
+        console.log('Start action: close exiftool');
+    }).catch( err => {
+        // tools1.write2Log('ERROR @default closing of exiftool: ' + err);
+    });
     // An ExifTool process is started to retrieve the metadata
     ep.open().then((pid) => {
         console.log('Started exiftool process %s', pid);
